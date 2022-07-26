434400f19e2c8662c7304a8af614a99bb6aaa8d3
Houssine <houssine199222@gmail.com>
2022-07-25 23:07:42 -0400

    Add function for computer to randomly select between rock paper or scissor

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    const computerChoice = Math.floor(Math.random()*3)+1;
+  switch(computerChoice){
+    case 1:
+        return "Rock";
+        break;
+    case 2:
+        return "Paper";
+        break;
+    case 3:
+        return "Scissor";
+        break;
+  }
+}
