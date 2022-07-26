5b17228cc327d432c2c2049ddd8e5abaa555ad97
Houssine <houssine199222@gmail.com>
2022-07-25 23:11:42 -0400

    Add comments on initial function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict"
+/*The code below randomly selects between "Rock" "Paper" or "Scissors"*/
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random()*3)+1;
   switch(computerChoice){
