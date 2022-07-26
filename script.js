f8c48e055a4ccedb287efa976b48bad8f9b0a45a
Houssine <houssine199222@gmail.com>
2022-07-26 01:18:02 -0400

    Add a function that plays a single round of rock, paper, scissor

--- script.js
+++ script.js
@@ -4,13 +4,35 @@ function getComputerChoice(){
     const computerChoice = Math.floor(Math.random()*3)+1;
   switch(computerChoice){
     case 1:
-        return "Rock";
+        return "rock";
         break;
     case 2:
-        return "Paper";
+        return "paper";
         break;
     case 3:
-        return "Scissor";
+        return "scissor";
         break;
   }
+}
+/*The player chooses between rock, paper, and scissor and competes against the computer in a single round*/
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        return alert("You lose! Paper beats rock.")
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissor"){
+        return alert("You win! Rock beats scissor.")
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissor"){
+        return alert("You lose! Scissor beats paper.")
+    }
+    else if(playerSelection == "paper" && computerSelection == "rock"){
+        return alert("You win! Paper beats rock.")
+    }
+   else if(playerSelection == "scissor" && computerSelection == "paper"){
+        return alert("You win! Scissor beats paper.")
+    }
+    else if(playerSelection == "scissor" && computerSelection == "rock"){
+        return alert("You lose! Rock beats scissor.")
+    }
+    else {return alert("It's a tie! No points gained.")}
 }
