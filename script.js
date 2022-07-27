52b8451c078943bbf181c7cf10cb1c6335d8caa6
Houssine <houssine199222@gmail.com>
2022-07-26 23:58:09 -0400

    Add function to play five rounds of rock, paper, scissor

--- script.js
+++ script.js
@@ -14,20 +14,39 @@ function getComputerChoice(){
         break;
   }
 }
+
 /*The player chooses between rock, paper, and scissor and competes against the computer in a single round*/
 function playRound(playerSelection, computerSelection){
-    if((playerSelection === "rock" && computerSelection === "scissor") || 
-    (playerSelection === "paper" && computerSelection === "rock" ) ||
-    (playerSelection === "rock" && computerSelection === "scissor")){
+    if((playerSelection === "rock".toLowerCase() && computerSelection === "scissor".toLowerCase()) || 
+    (playerSelection === "paper".toLowerCase() && computerSelection === "rock".toLowerCase()) ||
+    (playerSelection === "rock".toLowerCase() && computerSelection === "scissor".toLowerCase())){
         return "You win!"
     }
-    else if((playerSelection === "scissor" && computerSelection === "rock" )||
-    (playerSelection === "rock" && computerSelection === "paper" )||
-    (playerSelection === "scissor" && computerSelection === "rock" )){
+    else if((playerSelection === "scissor".toLowerCase() && computerSelection === "rock".toLowerCase())||
+    (playerSelection === "rock".toLowerCase() && computerSelection === "paper".toLowerCase())||
+    (playerSelection === "scissor".toLowerCase() && computerSelection === "rock".toLowerCase())){
         return "You lose!"
     }
-    else return "It's a tie!"
+    else {
+        return "It's a tie!"
+} 
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+let playerScore = 0;
+
+let computerScore = 0;
+
+/*Plays five rounds of rock, paper, and scissor*/
+function game(){
+    for (let i=0; i <5; i++){
+        
+        const playerSelection = prompt("Choose: rock, paper, or scissor?");
+        
+        const computerSelection = getComputerChoice();
+        
+        console.log(playRound(playerSelection, computerSelection));  
+    }        
+ }
+ 
+ game();
+
