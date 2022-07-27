0f828585c4caf2b367e67486170ae8ea264daa69
Houssine <houssine199222@gmail.com>
2022-07-26 21:31:14 -0400

    Tweak code to use less if and else if  statements

--- script.js
+++ script.js
@@ -16,23 +16,18 @@ function getComputerChoice(){
 }
 /*The player chooses between rock, paper, and scissor and competes against the computer in a single round*/
 function playRound(playerSelection, computerSelection){
-    if(playerSelection == "rock" && computerSelection == "paper"){
-        return alert("You lose! Paper beats rock.")
+    if((playerSelection === "rock" && computerSelection === "scissor") || 
+    (playerSelection === "paper" && computerSelection === "rock" ) ||
+    (playerSelection === "rock" && computerSelection === "scissor")){
+        return "You win!"
     }
-    else if(playerSelection == "rock" && computerSelection == "scissor"){
-        return alert("You win! Rock beats scissor.")
+    else if((playerSelection === "scissor" && computerSelection === "rock" )||
+    (playerSelection === "rock" && computerSelection === "paper" )||
+    (playerSelection === "scissor" && computerSelection === "rock" )){
+        return "You lose!"
     }
-    else if(playerSelection == "paper" && computerSelection == "scissor"){
-        return alert("You lose! Scissor beats paper.")
-    }
-    else if(playerSelection == "paper" && computerSelection == "rock"){
-        return alert("You win! Paper beats rock.")
-    }
-   else if(playerSelection == "scissor" && computerSelection == "paper"){
-        return alert("You win! Scissor beats paper.")
-    }
-    else if(playerSelection == "scissor" && computerSelection == "rock"){
-        return alert("You lose! Rock beats scissor.")
-    }
-    else {return alert("It's a tie! No points gained.")}
+    else return "It's a tie!"
 }
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
