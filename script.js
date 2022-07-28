7a73293973351722c406b27044fed9eec198c43e
Houssine <houssine199222@gmail.com>
2022-07-27 21:47:56 -0400

    Make minor tweaks and add additional comments

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
 "use strict"
+/* Player and Computer start with no points and gain points as they win rounds*/
+let playerScore = 0;
+let computerScore = 0;
+
 /*The code below randomly selects between "Rock" "Paper" or "Scissors"*/
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random()*3)+1;
   switch(computerChoice){
     case 1:
         return "rock";
-        break;
+        
     case 2:
         return "paper";
-        break;
+        
     case 3:
         return "scissor";
-        break;
+        
   }
 }
 
+
 /*The player chooses between rock, paper, and scissor and competes against the computer in a single round*/
 function playRound(playerSelection, computerSelection){
-    if((playerSelection === "rock".toLowerCase() && computerSelection === "scissor".toLowerCase()) || 
-    (playerSelection === "paper".toLowerCase() && computerSelection === "rock".toLowerCase()) ||
-    (playerSelection === "rock".toLowerCase() && computerSelection === "scissor".toLowerCase())){
-        return "You win!"
+    if ((playerSelection === "rock" && computerSelection === "scissor") || 
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissor" && computerSelection === "paper")) 
+    {
+        playerScore++;
+
+        return "You win!";
     }
-    else if((playerSelection === "scissor".toLowerCase() && computerSelection === "rock".toLowerCase())||
-    (playerSelection === "rock".toLowerCase() && computerSelection === "paper".toLowerCase())||
-    (playerSelection === "scissor".toLowerCase() && computerSelection === "rock".toLowerCase())){
-        return "You lose!"
+    else if ((playerSelection === "scissor" && computerSelection === "rock")||
+    (playerSelection === "rock" && computerSelection === "paper")||
+    (playerSelection === "paper" && computerSelection === "scissor"))
+    {
+
+        computerScore++;
+
+        return "You lose!";
     }
-    else {
-        return "It's a tie!"
-} 
+    else if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } 
+    else{
+        return "Please make an appropriate choice";
+    }
 }
 
-let playerScore = 0;
-
-let computerScore = 0;
 
 /*Plays five rounds of rock, paper, and scissor*/
 function game(){
-    for (let i=0; i <5; i++){
+    for (let i = 0; i < 5; i++){
         
-        const playerSelection = prompt("Choose: rock, paper, or scissor?");
+        const playerSelection = prompt("Choose: rock, paper, or scissor?").toLowerCase();
         
         const computerSelection = getComputerChoice();
         
         console.log(playRound(playerSelection, computerSelection));  
+        
+        console.log(`The current score is player:${playerScore} computer:${computerScore}`)
+        
+        
     }        
  }
  
  game();
 
+ 
